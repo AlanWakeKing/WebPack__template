312575aa2249c2f851f0afb8d29e6dfbe9386a83
Aleksey Kirsanov <lexamenz@ya.ru>
2024-05-29 15:30:28 +0300

    #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// index.js
+import './styles/scss/base.scss';
+
 const numbers = [2, 3, 5];
 const doubledNumbers = numbers.map(number => number * 2);
 console.log(doubledNumbers);
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,87 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// подключите к проекту mini-css-extract-plugin
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const loader = require("sass-loader");
+const { type } = require("os");
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-        publicPath: ''
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+    publicPath: "",
   },
-	mode: 'development',
-	devServer: {
-		static: path.resolve(__dirname, './dist'),
-		compress: true,
-		port: 8080,
-		open: true
-	},
-    module: {
-    rules: [ // rules — это массив правил
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
       // добавим в него объект правил для бабеля
       {
         // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
         // при обработке этих файлов нужно использовать babel-loader
-        use: 'babel-loader',
+        use: "babel-loader",
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/'
+        exclude: "/node_modules/",
       },
-			{test: /\.(png|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-				type: 'asset/resource'
-			},
-			{
-				test: /\.svg$/,
-				type: 'asset/resource/svg'
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				type: 'asset/resource/image'
-			},
-			{
-				test:/\.(woff(2)?|eot|ttf|otf)$/,
-				type: 'asset/resource/font'
-			}
-      ]
+      {
+        test: /\.svg$/,
+        type: "asset/resource/svg",
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: "asset/resource/image",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource/font",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, {
+          	loader: 'css-loader',
+          	options: { importLoaders: 1 }
+          },
+          'postcss-loader',
+          {
+          	loader: 'sass-loader',
+          	options: {
+          		sourceMap: true,
+          		sassOptions: {
+          			outputStyle: "compressed",
+          		}
+          	}
+          }
+        ],
+      },
+    ],
   },
-	plugins:[
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new CleanWebpackPlugin(),
-	]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "WebPack",
+      favicon: "./src/svg/favicon.svg",
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "WebPack-404",
+      favicon: "./src/svg/favicon.svg",
+      filename: "404.html",
+      template: "./src/error/404.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(
+			{filename: '[name].[hash].css'}
+		),
+  ],
 };
